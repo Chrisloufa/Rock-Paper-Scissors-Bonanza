70cc8a566329ea294dd8e67392cb43f4b41760c1
Chrisloufa <chris.mcgarry3@gmail.com>
2021-12-10 17:48:56 +0000

    format css + js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,169 +1,169 @@
 const game = () => {
-let pScore = 0;
-let cScore = 0;
+    let pScore = 0;
+    let cScore = 0;
 
-document.addEventListener("DOMContentLoaded", function () {
-    let allButtons = document.getElementsByTagName("button");
-    for (let button of allButtons) {
-        button.addEventListener("click", function () {
-            let userAnswer = this.getAttribute("data-type");
-            playGame(userAnswer);
+    document.addEventListener("DOMContentLoaded", function () {
+        let allButtons = document.getElementsByTagName("button");
+        for (let button of allButtons) {
+            button.addEventListener("click", function () {
+                let userAnswer = this.getAttribute("data-type");
+                playGame(userAnswer);
+                setTimeout(function () {
+                    returnGame(userAnswer);
+                }, 400);
+            });
+        }
+    });
+
+    function playGame(userAnswer) {
+
+        if (userAnswer === "Rock") {
+            displayUserRock();
+        } else if (userAnswer === "Paper") {
+            displayUserPaper();
+        } else if (userAnswer === "Scissors") {
+            displayUserScissors();
+        } else {
+            alert(`Unknown game type: ${userAnswer}`);
+            throw `Unknown game type: ${userAnswer}. Aborting!`;
+        }
+    }
+
+    /**
+     * Generates answer selected by user and displays correct image.
+     */
+
+    function displayUserRock() {
+
+        document.getElementById("user-image").src = "assets/images/user-rock.png";
+    }
+
+    function displayUserPaper() {
+
+        document.getElementById("user-image").src = "assets/images/user-paper.png";
+
+    }
+
+    function displayUserScissors() {
+
+        document.getElementById("user-image").src = "assets/images/user-scissors.png";
+    }
+
+    function returnGame(userAnswer) {
+
+        const updateScore = () => {
+            const playerScore = document.querySelector(".player-score");
+            const computerScore = document.querySelector(".computer-score");
+            playerScore.textContent = pScore;
+            computerScore.textContent = cScore;
+        };
+
+        let options = ["Rock", "Paper", "Scissors"];
+        let randomComputerAnswer = options[Math.floor(Math.random() * options.length)];
+
+        if (randomComputerAnswer === "Rock" && userAnswer === "Rock") {
+            displayComputerRock(randomComputerAnswer);
             setTimeout(function () {
-                returnGame(userAnswer);
-            }, 400);
-        });
-    }
-});
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+                updateScore();
+                return;
+            }, 10);
 
-function playGame(userAnswer) {
+        } else if (randomComputerAnswer === "Rock" && userAnswer === "Paper") {
+            displayComputerRock(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+                pScore++;
+                updateScore();
+                return;
+            }, 10);
 
-    if (userAnswer === "Rock") {
-        displayUserRock();
-    } else if (userAnswer === "Paper") {
-        displayUserPaper();
-    } else if (userAnswer === "Scissors") {
-        displayUserScissors();
-    } else {
-        alert(`Unknown game type: ${userAnswer}`);
-        throw `Unknown game type: ${userAnswer}. Aborting!`;
-    }
-}
-
-/**
- * Generates answer selected by user and displays correct image.
- */
-
-function displayUserRock() {
-
-    document.getElementById("user-image").src = "assets/images/user-rock.png";
-}
-
-function displayUserPaper() {
-
-    document.getElementById("user-image").src = "assets/images/user-paper.png";
-
-}
-
-function displayUserScissors() {
-
-    document.getElementById("user-image").src = "assets/images/user-scissors.png";
-}
-
-function returnGame(userAnswer) {
-
-    const updateScore = () => {
-        const playerScore = document.querySelector(".player-score");
-        const computerScore = document.querySelector(".computer-score");
-        playerScore.textContent = pScore;
-        computerScore.textContent = cScore;
-      };
-
-    let options = ["Rock", "Paper", "Scissors"];
-    let randomComputerAnswer = options[Math.floor(Math.random() * options.length)];
-
-    if (randomComputerAnswer === "Rock" && userAnswer === "Rock") {
-        displayComputerRock(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
-            updateScore();
-            return;
-        }, 10);
-
-    } else if (randomComputerAnswer === "Rock" && userAnswer === "Paper") {
-        displayComputerRock(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
-            pScore++;
-            updateScore();
-            return;
-        }, 10);
-
-    } else if (randomComputerAnswer === "Rock" && userAnswer === "Scissors") {
-        displayComputerRock(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
-            cScore++;
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Rock" && userAnswer === "Scissors") {
+            displayComputerRock(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
+                cScore++;
+                updateScore();
+                return;
+            }, 10);
 
 
-    } else if (randomComputerAnswer === "Paper" && userAnswer === "Rock") {
-        displayComputerPaper(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
-            cScore++;
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Paper" && userAnswer === "Rock") {
+            displayComputerPaper(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
+                cScore++;
+                updateScore();
+                return;
+            }, 10);
 
-    } else if (randomComputerAnswer === "Paper" && userAnswer === "Paper") {
-        displayComputerPaper(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Paper" && userAnswer === "Paper") {
+            displayComputerPaper(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+                updateScore();
+                return;
+            }, 10);
 
-    } else if (randomComputerAnswer === "Paper" && userAnswer === "Scissors") {
-        displayComputerPaper(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
-            pScore++;
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Paper" && userAnswer === "Scissors") {
+            displayComputerPaper(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+                pScore++;
+                updateScore();
+                return;
+            }, 10);
 
-    } else if (randomComputerAnswer === "Scissors" && userAnswer === "Rock") {
-        displayComputerScissors(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
-            pScore++;
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Scissors" && userAnswer === "Rock") {
+            displayComputerScissors(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou won!`);
+                pScore++;
+                updateScore();
+                return;
+            }, 10);
 
-    } else if (randomComputerAnswer === "Scissors" && userAnswer === "Paper") {
-        displayComputerScissors(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
-            cScore++;
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Scissors" && userAnswer === "Paper") {
+            displayComputerScissors(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nYou lost!`);
+                cScore++;
+                updateScore();
+                return;
+            }, 10);
 
-    } else if (randomComputerAnswer === "Scissors" && userAnswer === "Scissors") {
-        displayComputerScissors(randomComputerAnswer);
-        setTimeout(function () {
-            alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
-            updateScore();
-            return;
-        }, 10);
+        } else if (randomComputerAnswer === "Scissors" && userAnswer === "Scissors") {
+            displayComputerScissors(randomComputerAnswer);
+            setTimeout(function () {
+                alert(`You played: ${userAnswer} \nThe computer played: ${randomComputerAnswer} \nIt"s a draw!`);
+                updateScore();
+                return;
+            }, 10);
 
-    } else {
-        alert(`Game error, please try again later.`);
-        throw `Game error! Please investigate`;
+        } else {
+            alert(`Game error, please try again later.`);
+            throw `Game error! Please investigate`;
+        }
+
     }
 
-}
+    function displayComputerRock() {
 
-function displayComputerRock() {
+        document.getElementById("computer-image").src = "assets/images/computer-rock.png";
 
-    document.getElementById("computer-image").src = "assets/images/computer-rock.png";
+    }
 
-}
+    function displayComputerPaper() {
 
-function displayComputerPaper() {
+        document.getElementById("computer-image").src = "assets/images/computer-paper.png";
 
-    document.getElementById("computer-image").src = "assets/images/computer-paper.png";
+    }
 
-}
+    function displayComputerScissors() {
 
-function displayComputerScissors() {
+        document.getElementById("computer-image").src = "assets/images/computer-scissors.png";
 
-    document.getElementById("computer-image").src = "assets/images/computer-scissors.png";
-
-}
+    }
 }
 
 game();
@@ -171,4 +171,3 @@ game();
 /**
  * Gets the current tally of answers from the DOM and increments it by 1
  */
-
